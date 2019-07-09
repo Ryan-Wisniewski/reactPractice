ff8f6528c760919f19b509498f41e45b83fe7688
Ryan-Wisniewski <ryanwis98@yahoo.com>
2019-07-09 19:23:31 -0400

    timeout works/ clock works. but not together

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,23 +1,42 @@
 import React from 'react'
 
 function Footer(){
-    const date = new Date()
-    const hours = date.getHours()
+    let date = new Date()
+    let hours = date.getHours()
+    let minutes = date.getMinutes()
     let timeOfDay
-    console.log(hours)
-
+    console.log(hours, minutes)
+    // Checks hours for time of day and displays greeting
     if (hours < 12){
         timeOfDay = 'Good Morning!'
-    }else if (12 < timeOfDay < 18) {
+    }else if (12 < timeOfDay && timeOfDay < 18) {
         timeOfDay = `Good Afternoon!`
     }else {
         timeOfDay = 'Good Night!'
     }
 
+    //sets clock to standard time
+    let clock = `${hours}:${minutes}`
+    if (hours > 12){
+        clock = `0${hours - 12}:${minutes} `
+    } 
+    if (minutes < 10){
+        clock = `${hours - 12}:${0}${minutes}`
+    }
+    //refreshes clock every 20 seconds
+    function timedRefresh(timeoutPeriod){
+        setTimeout('location.reload(true);', timeoutPeriod)
+    }
+    document.onload = timedRefresh(5000)
+
     const styles = {color: "purple",backgroundColor: "gold", fontSize: 30}
 
     return (
-       <p style={styles}> Woah there! {timeOfDay}</p>
+        <div>
+            <p style={styles}> Woah there! {timeOfDay}</p>
+            <p style={styles}>The time is: {clock}</p>
+        </div>
+       
     )
 }
 
